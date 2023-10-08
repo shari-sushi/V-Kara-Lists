5900dd0199871be2b9382f40436de3d08e8aae20
sharin-sushi <naruazcom230@gmail.com>
2023-10-09 04:10:38 +0900

    一旦全てコミット

--- SESESE/0016go_next_relation/t0016Go/cmd/main.go
+++ SESESE/0016go_next_relation/t0016Go/cmd/main.go
@@ -44,22 +44,22 @@ func main() {
 	r.GET("/sings", crud.ReadAllSings)                //動作ok
 
 	// データ新規登録
-	r.POST("/create/vtuber", crud.CreateVtuber)     //未
-	r.POST("/create/movie", crud.CreateMovie)       //未
-	r.POST("/create/sings", crud.CreateKaraokeSing) //未
-	r.POST("/create/songs", crud.CreateSong)        //未　ver1.5かな
+	r.POST("/create/vtuber", crud.CreateVtuber)    //動作ok
+	r.POST("/create/movie", crud.CreateMovie)      //動作ok
+	r.POST("/create/sing", crud.CreateKaraokeSing) //動作ok
+	r.POST("/create/song", crud.CreateSong)        //未　ver1.5かな
 
 	//データ編集
-	r.POST("/edit/vtuber", crud.EditVtuber)     //未
-	r.POST("/edit/movie", crud.EditMovie)       //未
-	r.POST("/edit/sings", crud.EditKaraokeSing) //未
-	r.POST("/edit/songs", crud.EditSong)        //未　ver1.5かな
+	r.POST("/edit/vtuber", crud.EditVtuber)    //動作ok
+	r.POST("/edit/movie", crud.EditMovie)      //未
+	r.POST("/edit/sing", crud.EditKaraokeSing) //未
+	r.POST("/edit/song", crud.EditSong)        //未　ver1.5かな
 
 	// データ削除(論理)
-	r.DELETE("/delete/vtruber", crud.DeleteVtuber)    //未
-	r.DELETE("/delete/movie", crud.DeleteMovie)       //未
-	r.DELETE("/delete/sings", crud.DeleteKaraokeSing) //未
-	r.DELETE("/delete/songs", crud.DeleteSong)        //未　ver1.5かな
+	r.DELETE("/delete/vtruber", crud.DeleteVtuber)   //未
+	r.DELETE("/delete/movie", crud.DeleteMovie)      //未
+	r.DELETE("/delete/sing", crud.DeleteKaraokeSing) //未
+	r.DELETE("/delete/song", crud.DeleteSong)        //未　ver1.5かな
 
 	//ユーザー認証 ※ブラウザでは"/"にリンク有り
 	r.POST("/signup2", utility.CalltoSignUpHandler) //動作ほぼok　登録済みのメアドの時に、処理は止めてくれるけど、エラー内容を返してくれない…。
@@ -76,60 +76,6 @@ func main() {
 }
 
 // 多分消して良い
-// var streamer types.Vtuber
-// var streamers []types.Vtuber
-// var stsmos []*types.VtuberMovie //Scan()するからポインタ？
-
-// func FindAllStreamers(c *gin.Context) {
-// 	/////streamers 全件取得
-// 	resultSts := utility.Db.Find(&streamers)
-// 	if resultSts.Error != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"resultStsのerror": resultSts.Error.Error()})
-// 		return
-// 	}
-
-// 	fmt.Printf("全件取得streamers=%v \n", streamers)
-
-// 	////streamers, movies joinして全件取 ReadMoviesに同じ
-// 	resultStsmo := utility.Db.Model(&streamers).Select("streamers.streamer_id, streamers.streamer_name, m.movie_id, m.movie_url, m.movie_title").Joins("LEFT JOIN movies m USING(streamer_id)").Scan(&stsmos)
-// 	// SELECT ~略~ FROM streamers LEFT JOIN movies m USING streamer_id
-// 	if resultStsmo.Error != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"resultStsmoのerror": resultStsmo.Error.Error()})
-// 		return
-// 	}
-// 	// for resultStsmo.Next() {
-// 	// 	stsmos.Scan(&stsmo.Streamer.StreamerId, &stsmo.Streamer.StreamerName, &stsmo.Streamer.NameKana, &stsmo.Movie.MovieId, &stsmo.Movie.MovieUrl, &stsmo.Movie.MovieTitle)
-
-// 	// }
-// 	fmt.Printf("stsmos=%v, \n streamers=%v \n", stsmos, streamers)
-
-// 	for _, stsmo := range stsmos {
-// 		fmt.Printf("%+v\n", *stsmo)
-// 	}
-
-// 	c.JSON(http.StatusOK, gin.H{
-// 		"streamers":                  streamers,
-// 		"streamers_and_moviesmovies": stsmos,
-// 	})
-// }
-
-// SELECT streamers.streamer_id, streamers.streamer_name, m.movie_id, m.movie_url, m.movie_title FROM streamers LEFT JOIN movies m USING(streamer_id)
-
-// ********memo*******
-
-// セッション管理しようとしたときの。
-// type SessionInfo struct {
-// 	MemberId interface{}
-// 	// MemberName interface{}
-// }
-
-//https://qiita.com/koshi_an/items/12da955a1823b7f3e178より
-//ミドルウェア
-// func sessionCheck() gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 		var LoginInfo SessionInfo
-// 		session := sessions.Default(c)               //与えられたkeyの値が存在すればそれを返し、無ければpanic
-// 		LoginInfo.MemberId = session.Get("MemberId") //与えられたkeyに関連するsessionを返す
 
 // 		// セッションがない場合、ログインフォームをだす
 // 		if LoginInfo.MemberId == nil {
