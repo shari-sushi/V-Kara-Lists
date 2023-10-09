4e0bf1d09efc2840d64b37001ea752aef2dc4c49
sharin-sushi <naruazcom230@gmail.com>
2023-10-10 00:33:38 +0900

    0019の強制プッシュ。0019でのmerge全削除トラブルに起因。

---
+++ t0016Go/internal/utility/accesshowpage.go
@@ -0,0 +1,25 @@
+package utility
+
+// //"github.com/sharin-sushi/0022loginwithJWT/internal/utility"
+
+// import (
+// 	// "database/sql"
+// 	"fmt"
+// 	"net/http"
+// 	"os"
+
+// 	// "time"
+
+// 	"github.com/gin-gonic/gin"
+// 	_ "github.com/go-sql-driver/mysql"
+// 	"github.com/golang-jwt/jwt"
+// 	"github.com/sharin-sushi/0016go_next_relation/internal/controller/crypto"
+// 	"github.com/sharin-sushi/0016go_next_relation/internal/types"
+// 	"github.com/sharin-sushi/0016go_next_relation/internal/utility/token"
+// 	"github.com/sharin-sushi/0016go_next_relation/pkg/middleware"
+
+// 	"gorm.io/driver/mysql"
+// 	"gorm.io/gorm"
+// )
+
+// 将来的に、main関数からここに移植。/karaokelistとしてgroupする。
