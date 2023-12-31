e4c69adcf1e1cf2b02b03f0d060e81e919f83f2c
sharin-sushi <susi.imo.niku@gmail.com>
2024-01-01 02:06:50 +0900

    fix: next:何とか使える状態にはした #78

--- t0016Next/myapp/src/components/Conversion.tsx
+++ t0016Next/myapp/src/components/Conversion.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import style from '../Youtube.module.css';
-import type { AllJoinData, ReceivedVtuber, ReceivedMovie } from '../types/vtuber_content'; //type{}で型情報のみインポート
+import type { ReceivedVtuber, ReceivedMovie } from '../types/vtuber_content'; //type{}で型情報のみインポート
 import https from 'https';
 import axios from 'axios';
-import { YoutubePlayer } from '../components/YoutubePlayer'
 import { domain } from '../../env'
 
-export const ConversionTime = (SingStart: string): number => {
+export const ConvertStringToTime = (SingStart: string): number => {
+  console.log(SingStart)
   const match = SingStart.match(/\d+/g);
   if (!match || match.length !== 3) {
     console.error("Invalid input format:", SingStart);
@@ -16,14 +16,14 @@ export const ConversionTime = (SingStart: string): number => {
   const hours = Number(match[0]);
   const minutes = Number(match[1]);
   const seconds = Number(match[2]);
-  let totalSeconds = hours * 60 * 60 + minutes * 60 + seconds;
+  let totalSeconds = (hours * 60 * 60) + (minutes * 60) + seconds;
 
   console.log("totalSeconds:", totalSeconds);
   return totalSeconds;
 }
 
 
-export const ExtractVideoId = (url: string): string => { //(変数:型):返値の型
+export const ExtractVideoId = (url: string): string => {
   const match = url.match(/v=([^&]+)/);
   if (match && match[1]) {
     return match[1];
