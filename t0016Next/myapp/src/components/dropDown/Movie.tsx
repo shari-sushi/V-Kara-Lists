1ddf154151fcecc7f8c58769c7b50c466bac6a76
sharin-sushi <susi.imo.niku@gmail.com>
2024-01-03 15:32:20 +0900

    fix: next:vtuber, movie, karaoke 3つとも適応 #82

--- t0016Next/myapp/src/components/dropDown/Movie.tsx
+++ t0016Next/myapp/src/components/dropDown/Movie.tsx
@@ -4,7 +4,6 @@ import type { ReceivedVtuber, ReceivedMovie, ReceivedKaraoke } from '@/types/vtu
 import { DropStyle, TopPagePosts } from './common'
 
 // DropDinwMo, Kaについは、on~~Seletがnillとか0なら処理を止めべき
-
 type MovieOptions = {
     value: string;
     label: string;
@@ -12,45 +11,41 @@ type MovieOptions = {
 
 type DropDownMovieProps = {
     posts: TopPagePosts;
-    // posts:ReceivedMovie[];
     selectedVtuber: number;
-    onMovieSelect: (value: string) => void;
-    onKaraokeClear: (value: number) => void;
+    setSelectedMovie: (value: string) => void;
+    clearMovieHandler: () => void;
 };
 
-export const DropDownMovie = ({ posts, selectedVtuber, onMovieSelect, onKaraokeClear }: DropDownMovieProps) => {
-    const movies = posts?.vtubers_movies
-    //const [selectedVtuber, setSelectedVtuber] = useState(null);
+export const DropDownMovie = ({ posts, selectedVtuber, setSelectedMovie, clearMovieHandler }: DropDownMovieProps) => {
+    const movies = posts?.vtubers_movies || [{} as ReceivedMovie]
     const handleMovieClear = () => {
-        onMovieSelect("");
-        onKaraokeClear(0);
+        setSelectedMovie("");
+        clearMovieHandler();
     };
     const [movieOptions, setMovieOptions] = useState<MovieOptions[]>([]);
     const [selectedMovieId, setSelectedMovieId] = useState<string>("");
 
     useEffect(() => {
         if (!selectedVtuber) {
-            setMovieOptions([]); // Vtuberが選択解除された場合、movieの選択肢を空にする
+            setMovieOptions([]);
+            // ここで表示も消したい。消し方不明
             return;
-        }
-        const filterMoviesOfSelectedVtuber = async () => {
-            try {
-                console.log("selectedV=", selectedVtuber)
-                const choicesMovie = movies.filter((movies: ReceivedMovie) => movies.VtuberId === selectedVtuber);
-                console.log("API Response Mo:choicesMovie:", choicesMovie);
-                let havingMovies = choicesMovie.map((movie: ReceivedMovie) => ({
-                    value: movie.MovieUrl,
-                    label: movie.MovieTitle || ""
-                }));
-                console.log("havingMovie", havingMovies)
-                console.log("havingMovie.values", havingMovies.values)
-                setMovieOptions(havingMovies);
-            } catch (error) {
-                console.error("Error fetching movies:", error);
+        } else {
+            const filterMoviesBySelectedVtuber = async () => {
+                try {
+                    const movieDatum = movies.filter((movies: ReceivedMovie) => movies.VtuberId === selectedVtuber);
+                    const movieOptions = movieDatum.map((movie: ReceivedMovie) => ({
+                        value: movie.MovieUrl,
+                        label: movie.MovieTitle
+                    }));
+                    setMovieOptions(movieOptions);
+                } catch (error) {
+                    console.error("Error: failed to filter Movies By Selected Vtuber:", error);
+                };
+                setSelectedMovieId("");
             };
-            setSelectedMovieId("");
-        };
-        filterMoviesOfSelectedVtuber();
+            filterMoviesBySelectedVtuber();
+        }
     }, [selectedVtuber, movies]);
 
     return (
@@ -60,28 +55,22 @@ export const DropDownMovie = ({ posts, selectedVtuber, onMovieSelect, onKaraokeC
             placeholder="動画タイトルを検索/選択"
             className="basic-single"
             classNamePrefix="select"
-            // value={selectedMovie}
+            // value={""} //何を入れても選択したものが表示されないだけ
             isClearable={true}
             isSearchable={true}
             name="movie"
             blurInputOnSelect={true}  //defaultでtrueなら不要。スマホでアクセスしないと確認できないと思う。
-            captureMenuScroll={true} //スマホ用、タブレット用。使ってみてからt/f判断。
+            captureMenuScroll={true} //スマホ、タブレット用。使ってみてからt/f判断。
             styles={DropStyle}
-            // options={movieOptions}
             options={movieOptions}
             onChange={option => {
-                console.log("movieのvalue 前", selectedMovieId, "optin=", option?.value)
-                // 要：選択中のkaraokeをクリアする関数
-                if (option) { //option = {lavelu:string, value:string}であり、console.log("option", option.value)できる
-                    console.log("option", option.value)
-                    onMovieSelect(option.value);
+                if (option) {
+                    setSelectedMovie(option.value);
                     setSelectedMovieId(option.value);
-                    console.log("movieのvalue if(option)", selectedMovieId)
                 } else {
                     handleMovieClear();
                     setMovieOptions([]);
                 }
-                console.log("movieのvalue outside of if", selectedMovieId) //null 
             }}
         />
         </>
