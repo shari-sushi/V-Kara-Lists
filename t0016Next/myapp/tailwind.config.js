c31e4a037db57d130ce60573db6e8a5c4655bc1a
sharin-sushi <susi.imo.niku@gmail.com>
2024-01-12 23:29:29 +0900

    feat: tailwind使用できるよう設定 #85

--- t0016Next/myapp/tailwind.config.js
+++ t0016Next/myapp/tailwind.config.js
@@ -1,28 +1,64 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: 'jit',
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}",],
+  // content: [
+  //   "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  //   "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  //   "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  //   "./styles/**/*.{js,ts,jsx,tsx,mdx}",
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  //   // Or if using `src` directory:
+  //   "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  //   // "./components/layput/Layout.js",
 
+  // ],
+  // purge: {
+  //   content: [
+  //     // "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+  //     // "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  //     // "./src/**/**/*.{js,ts,jsx,tsx,mdx}",
+  //     // "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  //     // "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  //     // "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  //     // "./styles/**/*.{js,ts,jsx,tsx,mdx}",
 
-  ],
-  purge: {
-    content: [
-      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    options: {
-      // https://purgecss.com/safelisting.html#patterns
-      safelist: {
-        standard: [/^bg-/, /^text-/],
-      },
-    },
-  },
+  //     // Or if using `src` directory:
+  //     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  //     // "./components/layput/Layout.js",
+  //   ],
+  //   options: {
+  //     // https://purgecss.com/safelisting.html#patterns
+  //     safelist: {
+  //       standard: [/^bg-/, /^text-/],
+  //     },
+  //   },
+  // },
+  // content: [
+  //   "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  //   "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  //   "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  //   "./styles/**/*.{js,ts,jsx,tsx,mdx}",
+
+  //   // Or if using `src` directory:
+  //   "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  //   // "./components/layput/Layout.js",
+
+  // ],
+  // purge: {
+  //   content: [
+  //     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+  //     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  //     "./src/**/**/*.{js,ts,jsx,tsx,mdx}",
+  //   ],
+  //   options: {
+  //     // https://purgecss.com/safelisting.html#patterns
+  //     safelist: {
+  //       standard: [/^bg-/, /^text-/],
+  //     },
+  //   },
+  // },
+
   darkMode: true, // or 'media' or 'class'
   theme: {
     extend: {},
