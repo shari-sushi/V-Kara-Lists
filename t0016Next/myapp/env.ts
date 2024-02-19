337d510c06d9ea50b859755b61476e3ceda62900
sharin-sushi <susi.imo.niku@gmail.com>
2024-02-20 08:38:54 +0900

    go:corsにサブドメイン記述
    feat: go/next:疎通確認用testページ作成
    
    fix: next:クラウド用apiアクセスurlをhttpsに

--- t0016Next/myapp/env.ts
+++ t0016Next/myapp/env.ts
@@ -1,5 +1,8 @@
 export const domain = process.env.NODE_ENV === 'development' ? {
+  // VSCode上
   backendHost: 'http://localhost:8080/v1'
 } : {
-  backendHost: 'http://backend.v-karaoke.com/v1'
+  // クラウド環境
+  backendHost: 'https://backend.v-karaoke.com/v1'
+  // backendHost: 'http://localhost:8080/v1'
 }
