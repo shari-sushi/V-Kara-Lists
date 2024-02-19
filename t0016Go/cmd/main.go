8b240d4404417ce495329464d2ea691c26c1a30f
sharin-sushi <susi.imo.niku@gmail.com>
2024-02-20 08:51:03 +0900

    "rebase用"

--- t0016Go/cmd/main.go
+++ t0016Go/cmd/main.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-
 	"github.com/sharin-sushi/0016go_next_relation/infra"
 )
 
@@ -15,17 +14,19 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins: []string{
 			"http://localhost", "https://localhost",
-			"http://v-karaoke.com",
+			"http://v-karaoke.com", "https://v-karaoke.com",
+			"http://backend.v-karaoke.com", "https://backend.v-karaoke.com",
 		},
 		AllowMethods:     []string{"POST", "GET", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Cookie"},
 		AllowCredentials: true,
 	}))
 
+	fmt.Println("hallow api server in AWS 02.20")
+
 	infra.Routing(r)
 
 	r.Run(":8080")
-	// r.RunTLS(":8080", "../../key/server.pem", "../../key/server_unencrypted.key") //開発用
 }
 
 func requestLogger() gin.HandlerFunc {
