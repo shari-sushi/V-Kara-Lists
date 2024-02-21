63c20e26b8ed20656acd4b61157cf18af485d2f4
sharin-sushi <susi.imo.niku@gmail.com>
2024-02-21 22:45:45 +0900

    refator: comment #71

--- t0016Next/myapp/src/components/Conversion.tsx
+++ t0016Next/myapp/src/components/Conversion.tsx
@@ -1,5 +1,5 @@
 export const ConvertStringToTime = (SingStart: string): number => {
-  console.log(SingStart)
+  // console.log(SingStart)
   const match = SingStart.match(/\d+/g);
   if (!match || match.length !== 3) {
     console.error("Invalid input format:", SingStart);
@@ -10,10 +10,9 @@ export const ConvertStringToTime = (SingStart: string): number => {
   const seconds = Number(match[2]);
   let totalSeconds = (hours * 60 * 60) + (minutes * 60) + seconds;
 
-  console.log("totalSeconds:", totalSeconds);
+  // console.log("totalSeconds:", totalSeconds);
   return totalSeconds;
 }
-
 
 export const ExtractVideoId = (url: string): string => {
   const match = url.match(/v=([^&]+)/);
