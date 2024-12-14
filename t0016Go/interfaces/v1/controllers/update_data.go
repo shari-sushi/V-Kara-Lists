b442726ef6a376b7e62e54fb4dee00fe7dba934d
shari-sushi <susi.imo.niku@gmail.com>
2024-12-15 04:25:57 +0900

    お知らせ追加

--- t0016Go/interfaces/v1/controllers/update_data.go
+++ t0016Go/interfaces/v1/controllers/update_data.go
@@ -2,4 +2,4 @@ package controllers
 
 // 手動でmerge, buildしてる限りは、apiをbuildし直す日時をメモする（たまに忘れる）
 // 曲名１文字対応、等
-const LastUpdateData = "2024/11/23 15"
+const LastUpdateData = "2024/12/15 4時 fix:ローカルdb接続不具合"
