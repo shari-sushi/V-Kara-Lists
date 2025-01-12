sharin-sushi/0016go_next_relation

命名や型の修正

t0016Next/myapp/src/components/table/Karaoke.tsx
@@ -10,7 +10,7 @@ import axios from "axios";
 import Link from "next/link";
 
 import { domain } from "@/../env";
-import { ConvertStringToTime, ExtractVideoId } from "@/util";
+import { convertStringToTime, extractVideoId } from "@/util";
 import { shuffleArray } from "../SomeFunction";
 import { ReceivedKaraoke, FavoriteKaraoke } from "@/types/vtuber_content";
 import { ToDeleteContext } from "@/pages/crud/delete";
@@ -65,7 +65,7 @@ const columns: Column<ReceivedKaraoke>[] = [
           "https://" +
           row.original.MovieUrl +
           "&t=" +
-          ConvertStringToTime(row.original.SingStart);
+          convertStringToTime(row.original.SingStart);
         await navigator.clipboard.writeText(url);
         setIsDisplay(true);
         setTimeout(() => setIsDisplay(false), 2000);
@@ -78,7 +78,7 @@ const columns: Column<ReceivedKaraoke>[] = [
             onClick={() =>
               handleMovieClickYouTube(
                 row.original.MovieUrl,
-                ConvertStringToTime(row.original.SingStart)
+                convertStringToTime(row.original.SingStart)
               )
             }
           >
@@ -230,7 +230,7 @@ const PagenationReturnPostcolumns: Column<ReceivedKaraoke>[] = [
       const handleClickPlay = (post: ReceivedKaraoke) => {
         handleMovieClickYouTube(
           row.original.MovieUrl,
-          ConvertStringToTime(row.original.SingStart)
+          convertStringToTime(row.original.SingStart)
         );
         setSelectedPost(post);
       };
@@ -241,7 +241,7 @@ const PagenationReturnPostcolumns: Column<ReceivedKaraoke>[] = [
           "https://" +
           row.original.MovieUrl +
           "&t=" +
-          ConvertStringToTime(row.original.SingStart);
+          convertStringToTime(row.original.SingStart);
         await navigator.clipboard.writeText(url);
         setIsDisplay(true);
         setSelectedPost(row.original);
@@ -497,7 +497,7 @@ const ThinColumns: Column<ReceivedKaraoke>[] = [
           "https://" +
           row.original.MovieUrl +
           "&t=" +
-          ConvertStringToTime(row.original.SingStart);
+          convertStringToTime(row.original.SingStart);
         await navigator.clipboard.writeText(url);
         setIsDisplay(true);
         setTimeout(() => setIsDisplay(false), 2000);
@@ -510,7 +510,7 @@ const ThinColumns: Column<ReceivedKaraoke>[] = [
             onClick={() =>
               handleMovieClickYouTube(
                 row.original.MovieUrl,
-                ConvertStringToTime(row.original.SingStart)
+                convertStringToTime(row.original.SingStart)
               )
             }
           >
@@ -779,9 +779,9 @@ const deleteColumns: Column<ReceivedKaraoke>[] = [
       const { setCurrentVideoId, setCurrentStart } =
         useContext(ToDeleteContext);
       const clickHandler = (url: string, SingStart: string) => {
-        setCurrentVideoId(ExtractVideoId(url));
+        setCurrentVideoId(extractVideoId(url));
         // setTimeout(() => setCurrentStart(　// youtube iframバグ対策。再発に備えてコメントアウトで残しておく
-        ConvertStringToTime(SingStart);
+        convertStringToTime(SingStart);
         // ), 1450);
       };
       return (
@@ -869,7 +869,7 @@ const randam5columns: Column<ReceivedKaraoke>[] = [
       const handleClickSongName = (post: ReceivedKaraoke) => {
         handleMovieClickYouTube(
           post.MovieUrl,
-          ConvertStringToTime(post.SingStart)
+          convertStringToTime(post.SingStart)
         );
       };
 
@@ -879,7 +879,7 @@ const randam5columns: Column<ReceivedKaraoke>[] = [
           "https://" +
           row.original.MovieUrl +
           "&t=" +
-          ConvertStringToTime(row.original.SingStart);
+          convertStringToTime(row.original.SingStart);
         await navigator.clipboard.writeText(url);
         setIsDisplay(true);
         setTimeout(() => setIsDisplay(false), 2000);

t0016Next/myapp/src/pages/crud/delete.tsx
@@ -10,7 +10,7 @@ import type {
 } from "@/types/vtuber_content";
 import type { ContextType } from "@/types/server";
 import { YouTubePlayer } from "@/components/moviePlayer/YoutubePlayer";
-import { ConvertStringToTime, ExtractVideoId } from "@/util";
+import { convertStringToTime, extractVideoId } from "@/util";
 import { Layout } from "@/components/layout/Layout";
 import { VtuberDeleteTable } from "@/components/table/Vtuber";
 import { MovieDeleteTable } from "@/components/table/Movie";
@@ -82,7 +82,7 @@ export const DeletePage = ({ posts, isSignin }: Mypage) => {
         (movies) => movies.MovieUrl === toDeleteMovieUrl
       );
       if (foundMovie) {
-        const foundYoutubeId = ExtractVideoId(foundMovie.MovieUrl);
+        const foundYoutubeId = extractVideoId(foundMovie.MovieUrl);
         setCurrentVideoId(foundYoutubeId);
         setCurrentStart(1);
       }
@@ -95,7 +95,7 @@ export const DeletePage = ({ posts, isSignin }: Mypage) => {
         (karaoke) => karaoke.KaraokeId === toDeleteKaraokeId
       );
       if (foundKaraoke) {
-        const foundSingStart = ConvertStringToTime(foundKaraoke.SingStart);
+        const foundSingStart = convertStringToTime(foundKaraoke.SingStart);
         setCurrentStart(foundSingStart);
       }
     }

t0016Next/myapp/src/pages/sings/karaoke.tsx
@@ -13,7 +13,7 @@ import type {
 } from "@/types/vtuber_content";
 import type { ContextType } from "@/types/server";
 import { YouTubePlayer } from "@/components/moviePlayer/YoutubePlayer";
-import { ConvertStringToTime, ExtractVideoId } from "@/util";
+import { convertStringToTime, extractVideoId } from "@/util";
 import { DropDownVtuber } from "@/components/dropDown/Vtuber";
 import { DropDownMovie } from "@/components/dropDown/Movie";
 import KaraokeGlobalFilterTable from "@/components/table-tanstack/Karaoke/KaraokeGlobalFilterTable";
@@ -40,12 +40,12 @@ export default function SingsPage({ posts, isSignin }: TopPage) {
 
   // ようつべ用
   const primaryYoutubeUrl = "5WzeYsoGCZc"; //船長　kORHSmXcYNc, 00:08:29
-  const primaryYoutubeStartTime = ConvertStringToTime("00:22:04");
+  const primaryYoutubeStartTime = convertStringToTime("00:22:04");
   const [currentMovieId, setCurrentMovieId] =
     useState<string>(primaryYoutubeUrl);
   const [start, setStart] = useState<number>(primaryYoutubeStartTime);
   const handleMovieClickYouTube = (url: string, start: number) => {
-    setCurrentMovieId(ExtractVideoId(url));
+    setCurrentMovieId(extractVideoId(url));
     setStart(start);
   };
 

t0016Next/myapp/src/pages/sings/original-song.tsx
@@ -4,7 +4,7 @@ import { AxiosRequestConfig } from "axios";
 
 import type { ReceivedKaraoke } from "@/types/vtuber_content";
 import { YouTubePlayer } from "@/components/moviePlayer/YoutubePlayer";
-import { ConvertStringToTime, ExtractVideoId } from "@/util";
+import { convertStringToTime, extractVideoId } from "@/util";
 import { KaraokePagenatoinTable } from "@/components/table/Karaoke";
 import { Layout } from "@/components/layout/Layout";
 import { ContextType } from "@/types/server";
@@ -21,12 +21,12 @@ export default function SingsPage({ posts, isSignin }: PostsAndCheckSignin) {
   const karaokes = posts?.vtubers_movies_karaokes || ([] as ReceivedKaraoke[]);
 
   const primaryYoutubeUrl = "HcpFGZNusBw"; //船長　kORHSmXcYNc, 00:08:29
-  const primaryYoutubeStartTime = ConvertStringToTime("");
+  const primaryYoutubeStartTime = convertStringToTime("");
   const [currentMovieId, setCurrentMovieId] =
     useState<string>(primaryYoutubeUrl);
   const [start, setStart] = useState<number>(primaryYoutubeStartTime);
   const handleMovieClickYouTube = (url: string, start: number) => {
-    setCurrentMovieId(ExtractVideoId(url));
+    setCurrentMovieId(extractVideoId(url));
     setStart(start);
   };
 

t0016Next/myapp/src/pages/vtuber/[vtuber_kana].tsx
@@ -8,7 +8,7 @@ import { Layout } from "@/components/layout/Layout";
 import { ToClickTW } from "@/styles/tailwiind";
 import type { ReceivedKaraoke, ReceivedMovie } from "@/types/vtuber_content";
 import { YouTubePlayer } from "@/components/moviePlayer/YoutubePlayer";
-import { ConvertStringToTime, ExtractVideoId } from "@/util";
+import { convertStringToTime, extractVideoId } from "@/util";
 import { DropDownAllMovie } from "@/components/dropDown/Movie";
 import { NotFoundVtuber } from "@/components/layout/Main";
 import { GetServerSidePropsContext } from "next";
@@ -37,10 +37,10 @@ export default function VtuberOriginalPage({ posts, isSignin }: VtuberPage) {
   // TODO: ロジック正しいか確認。動画がkaraokesから選ばれてtimeが初期値になることはないか。動画は0番目や最終番目も選ばれるか。
   const url = "www.youtube.com/watch?v=kORHSmXcYNc"; // 船長
   const stringTime = "00:08:29"; // ピンクレディー メドレー
-  const primaryYoutubeUrl = ExtractVideoId(
+  const primaryYoutubeUrl = extractVideoId(
     karaokes[playKaraokeNumber]?.MovieUrl || url
   );
-  const primaryYoutubeStartTime = ConvertStringToTime(
+  const primaryYoutubeStartTime = convertStringToTime(
     karaokes[playKaraokeNumber]?.SingStart || stringTime
   );
 
@@ -49,7 +49,7 @@ export default function VtuberOriginalPage({ posts, isSignin }: VtuberPage) {
   const [start, setStart] = useState<number>(primaryYoutubeStartTime);
 
   const handleMovieClickYouTube = (url: string, start: number) => {
-    setCurrentMovieId(ExtractVideoId(url));
+    setCurrentMovieId(extractVideoId(url));
     setStart(start);
   };
 

t0016Next/myapp/src/util/convertion/convert.ts
@@ -1,4 +1,4 @@
-export const ConvertStringToTime = (SingStart: string): number => {
+export const convertStringToTime = (SingStart: string): number => {
   const match = SingStart.match(/\d+/g);
   if (!match || match.length !== 3) {
     console.error("Invalid input format:", SingStart);
@@ -11,7 +11,7 @@ export const ConvertStringToTime = (SingStart: string): number => {
   return totalSeconds;
 };
 
-export const ExtractVideoId = (url: string): string => {
+export const extractVideoId = (url: string): string => {
   const match = url.match(/v=([^&]+)/);
   if (match && match[1]) {
     return match[1];

shari-sushi/V-Kara-Lists

t0016Next/myapp/src/components/table/Karaoke.tsx
@@ -10,7 +10,7 @@ import axios from "axios";
 import Link from "next/link";
 
 import { domain } from "@/../env";
-import { ConvertStringToTime, ExtractVideoId } from "@/util";
+import { timeStringToSecondNum, extractVideoId } from "@/util";
 import { shuffleArray } from "../SomeFunction";
 import { ReceivedKaraoke, FavoriteKaraoke } from "@/types/vtuber_content";
 import { ToDeleteContext } from "@/pages/crud/delete";
@@ -65,7 +65,7 @@ const columns: Column<ReceivedKaraoke>[] = [
           "https://" +
           row.original.MovieUrl +
           "&t=" +
-          ConvertStringToTime(row.original.SingStart);
+          timeStringToSecondNum(row.original.SingStart);
         await navigator.clipboard.writeText(url);
         setIsDisplay(true);
         setTimeout(() => setIsDisplay(false), 2000);
@@ -78,7 +78,7 @@ const columns: Column<ReceivedKaraoke>[] = [
             onClick={() =>
               handleMovieClickYouTube(
                 row.original.MovieUrl,
-                ConvertStringToTime(row.original.SingStart)
+                timeStringToSecondNum(row.original.SingStart)
               )
             }
           >
@@ -230,7 +230,7 @@ const PagenationReturnPostcolumns: Column<ReceivedKaraoke>[] = [
       const handleClickPlay = (post: ReceivedKaraoke) => {
         handleMovieClickYouTube(
           row.original.MovieUrl,
-          ConvertStringToTime(row.original.SingStart)
+          timeStringToSecondNum(row.original.SingStart)
         );
         setSelectedPost(post);
       };
@@ -241,7 +241,7 @@ const PagenationReturnPostcolumns: Column<ReceivedKaraoke>[] = [
           "https://" +
           row.original.MovieUrl +
           "&t=" +
-          ConvertStringToTime(row.original.SingStart);
+          timeStringToSecondNum(row.original.SingStart);
         await navigator.clipboard.writeText(url);
         setIsDisplay(true);
         setSelectedPost(row.original);
@@ -497,7 +497,7 @@ const ThinColumns: Column<ReceivedKaraoke>[] = [
           "https://" +
           row.original.MovieUrl +
           "&t=" +
-          ConvertStringToTime(row.original.SingStart);
+          timeStringToSecondNum(row.original.SingStart);
         await navigator.clipboard.writeText(url);
         setIsDisplay(true);
         setTimeout(() => setIsDisplay(false), 2000);
@@ -510,7 +510,7 @@ const ThinColumns: Column<ReceivedKaraoke>[] = [
             onClick={() =>
               handleMovieClickYouTube(
                 row.original.MovieUrl,
-                ConvertStringToTime(row.original.SingStart)
+                timeStringToSecondNum(row.original.SingStart)
               )
             }
           >
@@ -779,9 +779,9 @@ const deleteColumns: Column<ReceivedKaraoke>[] = [
       const { setCurrentVideoId, setCurrentStart } =
         useContext(ToDeleteContext);
       const clickHandler = (url: string, SingStart: string) => {
-        setCurrentVideoId(ExtractVideoId(url));
+        setCurrentVideoId(extractVideoId(url));
         // setTimeout(() => setCurrentStart(　// youtube iframバグ対策。再発に備えてコメントアウトで残しておく
-        ConvertStringToTime(SingStart);
+        timeStringToSecondNum(SingStart);
         // ), 1450);
       };
       return (
@@ -869,7 +869,7 @@ const randam5columns: Column<ReceivedKaraoke>[] = [
       const handleClickSongName = (post: ReceivedKaraoke) => {
         handleMovieClickYouTube(
           post.MovieUrl,
-          ConvertStringToTime(post.SingStart)
+          timeStringToSecondNum(post.SingStart)
         );
       };
 
@@ -879,7 +879,7 @@ const randam5columns: Column<ReceivedKaraoke>[] = [
           "https://" +
           row.original.MovieUrl +
           "&t=" +
-          ConvertStringToTime(row.original.SingStart);
+          timeStringToSecondNum(row.original.SingStart);
         await navigator.clipboard.writeText(url);
         setIsDisplay(true);
         setTimeout(() => setIsDisplay(false), 2000);

t0016Next/myapp/src/pages/crud/delete.tsx
@@ -10,7 +10,7 @@ import type {
 } from "@/types/vtuber_content";
 import type { ContextType } from "@/types/server";
 import { YouTubePlayer } from "@/components/moviePlayer/YoutubePlayer";
-import { ConvertStringToTime, ExtractVideoId } from "@/util";
+import { timeStringToSecondNum, extractVideoId } from "@/util";
 import { Layout } from "@/components/layout/Layout";
 import { VtuberDeleteTable } from "@/components/table/Vtuber";
 import { MovieDeleteTable } from "@/components/table/Movie";
@@ -82,7 +82,7 @@ export const DeletePage = ({ posts, isSignin }: Mypage) => {
         (movies) => movies.MovieUrl === toDeleteMovieUrl
       );
       if (foundMovie) {
-        const foundYoutubeId = ExtractVideoId(foundMovie.MovieUrl);
+        const foundYoutubeId = extractVideoId(foundMovie.MovieUrl);
         setCurrentVideoId(foundYoutubeId);
         setCurrentStart(1);
       }
@@ -95,7 +95,7 @@ export const DeletePage = ({ posts, isSignin }: Mypage) => {
         (karaoke) => karaoke.KaraokeId === toDeleteKaraokeId
       );
       if (foundKaraoke) {
-        const foundSingStart = ConvertStringToTime(foundKaraoke.SingStart);
+        const foundSingStart = timeStringToSecondNum(foundKaraoke.SingStart);
         setCurrentStart(foundSingStart);
       }
     }

t0016Next/myapp/src/pages/sings/karaoke.tsx
@@ -13,7 +13,7 @@ import type {
 } from "@/types/vtuber_content";
 import type { ContextType } from "@/types/server";
 import { YouTubePlayer } from "@/components/moviePlayer/YoutubePlayer";
-import { ConvertStringToTime, ExtractVideoId } from "@/util";
+import { timeStringToSecondNum, extractVideoId } from "@/util";
 import { DropDownVtuber } from "@/components/dropDown/Vtuber";
 import { DropDownMovie } from "@/components/dropDown/Movie";
 import KaraokeGlobalFilterTable from "@/components/table-tanstack/Karaoke/KaraokeGlobalFilterTable";
@@ -40,12 +40,12 @@ export default function SingsPage({ posts, isSignin }: TopPage) {
 
   // ようつべ用
   const primaryYoutubeUrl = "5WzeYsoGCZc"; //船長　kORHSmXcYNc, 00:08:29
-  const primaryYoutubeStartTime = ConvertStringToTime("00:22:04");
+  const primaryYoutubeStartTime = timeStringToSecondNum("00:22:04");
   const [currentMovieId, setCurrentMovieId] =
     useState<string>(primaryYoutubeUrl);
   const [start, setStart] = useState<number>(primaryYoutubeStartTime);
   const handleMovieClickYouTube = (url: string, start: number) => {
-    setCurrentMovieId(ExtractVideoId(url));
+    setCurrentMovieId(extractVideoId(url));
     setStart(start);
   };
 

t0016Next/myapp/src/pages/sings/original-song.tsx
@@ -4,7 +4,7 @@ import { AxiosRequestConfig } from "axios";
 
 import type { ReceivedKaraoke } from "@/types/vtuber_content";
 import { YouTubePlayer } from "@/components/moviePlayer/YoutubePlayer";
-import { ConvertStringToTime, ExtractVideoId } from "@/util";
+import { timeStringToSecondNum, extractVideoId } from "@/util";
 import { KaraokePagenatoinTable } from "@/components/table/Karaoke";
 import { Layout } from "@/components/layout/Layout";
 import { ContextType } from "@/types/server";
@@ -21,12 +21,12 @@ export default function SingsPage({ posts, isSignin }: PostsAndCheckSignin) {
   const karaokes = posts?.vtubers_movies_karaokes || ([] as ReceivedKaraoke[]);
 
   const primaryYoutubeUrl = "HcpFGZNusBw"; //船長　kORHSmXcYNc, 00:08:29
-  const primaryYoutubeStartTime = ConvertStringToTime("");
+  const primaryYoutubeStartTime = timeStringToSecondNum("");
   const [currentMovieId, setCurrentMovieId] =
     useState<string>(primaryYoutubeUrl);
   const [start, setStart] = useState<number>(primaryYoutubeStartTime);
   const handleMovieClickYouTube = (url: string, start: number) => {
-    setCurrentMovieId(ExtractVideoId(url));
+    setCurrentMovieId(extractVideoId(url));
     setStart(start);
   };
 

t0016Next/myapp/src/pages/vtuber/[vtuber_kana].tsx
@@ -8,7 +8,7 @@ import { Layout } from "@/components/layout/Layout";
 import { ToClickTW } from "@/styles/tailwiind";
 import type { ReceivedKaraoke, ReceivedMovie } from "@/types/vtuber_content";
 import { YouTubePlayer } from "@/components/moviePlayer/YoutubePlayer";
-import { ConvertStringToTime, ExtractVideoId } from "@/util";
+import { timeStringToSecondNum, extractVideoId } from "@/util";
 import { DropDownAllMovie } from "@/components/dropDown/Movie";
 import { NotFoundVtuber } from "@/components/layout/Main";
 import { GetServerSidePropsContext } from "next";
@@ -37,10 +37,10 @@ export default function VtuberOriginalPage({ posts, isSignin }: VtuberPage) {
   // TODO: ロジック正しいか確認。動画がkaraokesから選ばれてtimeが初期値になることはないか。動画は0番目や最終番目も選ばれるか。
   const url = "www.youtube.com/watch?v=kORHSmXcYNc"; // 船長
   const stringTime = "00:08:29"; // ピンクレディー メドレー
-  const primaryYoutubeUrl = ExtractVideoId(
+  const primaryYoutubeUrl = extractVideoId(
     karaokes[playKaraokeNumber]?.MovieUrl || url
   );
-  const primaryYoutubeStartTime = ConvertStringToTime(
+  const primaryYoutubeStartTime = timeStringToSecondNum(
     karaokes[playKaraokeNumber]?.SingStart || stringTime
   );
 
@@ -49,7 +49,7 @@ export default function VtuberOriginalPage({ posts, isSignin }: VtuberPage) {
   const [start, setStart] = useState<number>(primaryYoutubeStartTime);
 
   const handleMovieClickYouTube = (url: string, start: number) => {
-    setCurrentMovieId(ExtractVideoId(url));
+    setCurrentMovieId(extractVideoId(url));
     setStart(start);
   };
 

t0016Next/myapp/src/util/convertion/convert.ts
@@ -1,4 +1,4 @@
-export const ConvertStringToTime = (SingStart: string): number => {
+export const timeStringToSecondNum = (SingStart: string): number => {
   const match = SingStart.match(/\d+/g);
   if (!match || match.length !== 3) {
     console.error("Invalid input format:", SingStart);
@@ -11,7 +11,7 @@ export const ConvertStringToTime = (SingStart: string): number => {
   return totalSeconds;
 };
 
-export const ExtractVideoId = (url: string): string => {
+export const extractVideoId = (url: string): string => {
   const match = url.match(/v=([^&]+)/);
   if (match && match[1]) {
     return match[1];