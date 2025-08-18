4efa99ad35b6a7042d446800eccab5923609160a
shari-sushi <susi.imo.niku@gmail.com>
2025-08-18 09:32:14 +0900

    wip

--- t0016Next/myapp/src/components/moviePlayer/ReactPlayer.tsx
+++ t0016Next/myapp/src/components/moviePlayer/ReactPlayer.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState, useId } from "react";
 import ReactPlayer from "react-player";
 
 interface YoutubePlayerProps {
@@ -11,6 +11,16 @@ interface YoutubePlayerProps {
 }
 
 export const YoutubePlayer = ({ videoId, start, style }: YoutubePlayerProps) => {
+  const playerRef = useRef<HTMLMediaElement | null>(null);
+
+  // だめだった。toSeekもだめだしなんなん？
+  // 動画を指定した位置から再生するFunc
+  const offSetStart = (offSetSeconds: number) => {
+    if (playerRef != null && playerRef.current != null) {
+      playerRef.current.fastSeek(offSetSeconds);
+    }
+  };
+
   return (
     // TODO: startが変わるたびに動画が再読み込みになってしまうので、正規の方法を探す
     //       startが同じなら無反応になるし
@@ -20,6 +30,9 @@ export const YoutubePlayer = ({ videoId, start, style }: YoutubePlayerProps) => 
         height={style.height}
         width={style.width}
         controls
+        onReady={() => {
+          offSetStart(start);
+        }}
         playing
         config={{
           youtube: {
