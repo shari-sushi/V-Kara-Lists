871f43bd5bea25a7e6f0854421110784a8af4c17
shari-sushi <susi.imo.niku@gmail.com>
2025-12-05 18:22:10 +0900

    ga4入れてみた

--- t0016Next/myapp/src/pages/_app.js
+++ t0016Next/myapp/src/pages/_app.js
@@ -1,16 +1,49 @@
 import "../styles/global.css"
 import { RootProvider } from "@/providers/RootProvider"
 import { VideoLayout } from "@/components/layout/VideoLayout"
+import Script from "next/script"
+import { useEffect } from "react"
+import { useRouter } from "next/router"
 
 export default function App({ Component, pageProps }) {
   const isSignin = pageProps.isSignin || false
+  const router = useRouter()
+
+  // SPA ページ遷移で PV を飛ばす
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (typeof window !== "undefined" && window.gtag) {
+        window.gtag("config", "G-XZY96J18P3", {
+          page_path: url,
+        })
+      }
+    }
+
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
 
   return (
-    <RootProvider isSignin={isSignin}>
-      <div className="h-full w-full relative">
-        <Component {...pageProps} />
-        <VideoLayout />
-      </div>
-    </RootProvider>
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-XZY96J18P3" strategy="afterInteractive" />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-XZY96J18P3');
+        `}
+      </Script>
+
+      <RootProvider isSignin={isSignin}>
+        <div className="h-full w-full relative">
+          <Component {...pageProps} />
+          <VideoLayout />
+        </div>
+      </RootProvider>
+    </>
   )
 }
